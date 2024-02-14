8bc74580351194859d22b75397b1042f30dc976d
yurikahirata <yurikahirata0130@gmail.com>
2024-02-14 17:08:53 +0900

    fix: changed fetch url to localhost

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch('https://wolf-backend.onrender.com/login', {
+      const response = await fetch('http://localhost:8080/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
