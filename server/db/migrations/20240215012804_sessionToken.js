e88640dfbbc50dfcae88a1d163306ad8ea6caf87
yurikahirata <yurikahirata0130@gmail.com>
2024-02-15 11:45:06 +0900

    feat: img migration and seed in post_table

---
+++ server/db/migrations/20240215012804_sessionToken.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.table("user_table", function (table) {
+    table.string("sessionToken");
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.table("user_table", function (table) {
+    table.dropColumn("sessionToken");
+  })
+};
