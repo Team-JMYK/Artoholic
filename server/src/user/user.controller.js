9743e7ac79d327d17470021a3eb6153c65965f44
ShizuokaTerrier <matt@squareoneJapan.com>
2024-02-15 10:36:50 +0900

    feat: MVC complete

--- server/src/user/user.controller.js
+++ server/src/user/user.controller.js
@@ -30,9 +30,28 @@ module.exports = {
                 image,
             } = req.body;
 
-            const newUserData = await userModel.addNewUser
+            const newUserData = await userModel.addNewUser()
+            res.send(newUserData)
         } catch (error) {
-            
+            console.log(error.message)
         }
-    }
+    },
+    async deleteAUser(req,res){
+        try {
+            const id = parseInt(req.params.id);
+            const deleteTheUser = await userModel.deleteUser(id);
+            res.send(`User ${id} was deleted`)
+        } catch (error) {
+            console.log(error.message);
+        }
+    },
+    async updateAUser(req,res){
+        try {
+            const id = parseInt(req.params.id);
+            const updateTheUser = await userModel.updateUser(id);
+            res.send(updateTheUser)
+        } catch (error) {
+            console.log(error.message)
+        }
+    },
 }
